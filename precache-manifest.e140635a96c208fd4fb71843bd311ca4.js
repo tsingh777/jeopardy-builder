d8eb2a39d34d442564665a12875ed3b8121d37d4
Taranjit Singh <tsingh777@gmail.com>
2020-04-16 18:43:45 -0700

    release

---
+++ precache-manifest.e140635a96c208fd4fb71843bd311ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d0282204f15dddfdfc8b8ea911f256",
+    "url": "/jeopardy-builder/index.html"
+  },
+  {
+    "revision": "b9de21197e443eb21359",
+    "url": "/jeopardy-builder/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "34385ff9a67ca3275aa6",
+    "url": "/jeopardy-builder/static/js/2.7ffa4a8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jeopardy-builder/static/js/2.7ffa4a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9de21197e443eb21359",
+    "url": "/jeopardy-builder/static/js/main.2562434d.chunk.js"
+  },
+  {
+    "revision": "df7f58fbe55557434e47",
+    "url": "/jeopardy-builder/static/js/runtime-main.fdfb8cb4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21ddd028d401db421934c793a6fd976f.js"
+  "/jeopardy-builder/precache-manifest.e140635a96c208fd4fb71843bd311ca4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jeopardy-builder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
